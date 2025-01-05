65fc744205dab5c38b26bacef624cd6d8e0dafe4
Shizzy-ctrl <pawelgren462@gmail.com>
2025-01-05 13:55:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD test"
+    return "CI/CD test auto image"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
